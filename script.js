2feefeb17eef6e44a7d9fa61589c0fb0d6ae28b8
Ogdoabyss <101399129+YingHi@users.noreply.github.com>
2022-04-20 10:36:45 +0900

    교수님의 억까를 이겨냈다.
    워후~

--- script.js
+++ script.js
@@ -1,113 +1,111 @@
-var p1 = document.getElementById("p1");
-      var p2 = document.getElementById("p2");
-      var p3 = document.getElementById("p3");
-      var div1 = document.getElementById("div1");
-      var div2 = document.getElementById("div2");
-      var div3 = document.getElementById("div3");
-      var diva1, diva2, diva3;
-      div1.addEventListener('click',focus1)
-      div2.addEventListener('click',focus2)
-      div3.addEventListener('click',focus3)
-      var btn = document.getElementById("btn");
-      btn.addEventListener('click',Re)
-      var focused = 0;
 
-      function Re(){
-        focused = 0;
-        diva1 = new Array();
-        diva2 = new Array();
-        diva3 = new Array();
-        diva1.push("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
-        diva1.push("🟨🟨🟨🟨🟨🟨🟨🟨")
-        diva1.push("🟩🟩🟩🟩🟩🟩")
-        diva1.push("🟦🟦🟦🟦")
-        diva1.push("🟪🟪🟪")
-        diva1.push("⬛️")
-        update();
-      }
+var div1 = document.getElementById("div1");
+var div2 = document.getElementById("div2");
+var div3 = document.getElementById("div3");
+var diva1, diva2, diva3;
+div1.addEventListener('click',focus1)
+div2.addEventListener('click',focus2)
+div3.addEventListener('click',focus3)
+var btn = document.getElementById("btn");
+btn.addEventListener('click',Re)
+var focused = 0;
 
-      function focus1(){
-        if(focused == 0){
-          div1.id = "focus"
-          focused = 1
-        }else{
-          if(focused == 2){
-            diva1.push(diva2.pop())
-          }
-          else if(focused == 3){
-            diva1.push(diva3.pop())
-          }
-          update()
-        }
-      }
-      function focus2(){
-        if(focused == 0){
-          div2.id = "focus"
-          focused = 2
-        }else{
-          if(focused == 1){
-            diva2.push(diva1.pop())
-          }
-          else if(focused == 3){
-            diva2.push(diva3.pop())
-          }
-          update()
-        }
-      }
-      function focus3(){
-        if(focused == 0){
-          div3.id = "focus"
-          focused = 3
-        }else{
-          if(focused == 2){
-            diva3.push(diva2.pop())
-          }
-          else if(focused == 1){
-            diva3.push(diva1.pop())
-          }
-          update()
-        }
-      }
+function Re(){
+focused = 0;
+diva1 = new Array();
+diva2 = new Array();
+diva3 = new Array();
+diva1.push("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+diva1.push("🟨🟨🟨🟨🟨🟨🟨🟨")
+diva1.push("🟩🟩🟩🟩🟩🟩")
+diva1.push("🟦🟦🟦🟦")
+diva1.push("🟪🟪🟪")
+diva1.push("⬛️")
+update();
+}
 
-      function update(){
-        div1.id = "div1"
-        div2.id = "div2"
-        div3.id = "div3"
-        focused = 0
+function focus1(){
+if(focused == 0){
+    div1.id = "focus"
+    focused = 1
+}else{
+    if(focused == 2){
+      diva1.push(diva2.pop())
+    }
+    else if(focused == 3){
+      diva1.push(diva3.pop())
+    }
+    update()
+}
+}
+function focus2(){
+if(focused == 0){
+    div2.id = "focus"
+    focused = 2
+}else{
+    if(focused == 1){
+       diva2.push(diva1.pop())
+    }
+    else if(focused == 3){
+      diva2.push(diva3.pop())
+    }
+    update()
+}
+}
+function focus3(){
+if(focused == 0){
+    div3.id = "focus"
+    focused = 3
+}else{
+    if(focused == 2){
+      diva3.push(diva2.pop())
+    }
+    else if(focused == 1){
+      diva3.push(diva1.pop())
+    }
+    update()
+}
+}
 
-        for(let i = 0; i < diva1.length; i++) {
-            if(diva1[i] === undefined)  {
-              diva1.splice(i, 1);
-              i--;
-            }
-          }
-        for(let i = 0; i < diva2.length; i++) {
-          if(diva2[i] === undefined)  {
-            diva2.splice(i, 1);
-            i--;
-          }
-        }
-        for(let i = 0; i < diva3.length; i++) {
-          if(diva3[i] === undefined)  {
-            diva3.splice(i, 1);
-            i--;
-          }
-        }
+function update(){
+div1.id = "div1"
+div2.id = "div2"
+div3.id = "div3"
+focused = 0
 
-        p1.innerHTML = diva1.join("<br>")
-        p2.innerHTML = diva2.join("<br>")
-        p3.innerHTML = diva3.join("<br>")
+for(let i = 0; i < diva1.length; i++) {
+    if(diva1[i] === undefined)  {
+        diva1.splice(i, 1);
+        i--;
+    }
+    }
+for(let i = 0; i < diva2.length; i++) {
+    if(diva2[i] === undefined)  {
+        diva2.splice(i, 1);
+        i--;
+    }
+}
+for(let i = 0; i < diva3.length; i++) {
+    if(diva3[i] === undefined)  {
+        diva3.splice(i, 1);
+        i--;
+    }
+}
 
-        if(diva1[0] == null || undefined){
-          p1.innerHTML = "first"
-        }
-        if(diva2[0] == null || undefined){
-          p2.innerHTML = "second"
-        }
-        if(diva3[0] == null || undefined){
-          p3.innerHTML = "third"
-        }
+div1.innerHTML = diva1.join("<br>")
+div2.innerHTML = diva2.join("<br>")
+div3.innerHTML = diva3.join("<br>")
 
-        }
+if(diva1[0] == null || undefined){
+    div1.innerHTML = "first"
+}
+if(diva2[0] == null || undefined){
+    div2.innerHTML = "second"
+}
+if(diva3[0] == null || undefined){
+    div3.innerHTML = "third"
+}
 
-      window.onload = Re;
+}
+
+window.onload = Re;
